e200d2484789b6bbed5d2e392f2be9c50d93121e
w311ang <30381083+w311ang@users.noreply.github.com>
2021-11-05 17:51:00 +0800

    Update main.py

--- main.py
+++ main.py
@@ -12,5 +12,5 @@ with s.post('https://woapp.10155.com/trafficBus/userAppList/checkUserflow') as r
   password=result['password']
   socksurl='socks5://%s:%s@%s:%s/s'%(username,password,host,port)
   #socks5://3000007591&13135361530&1636100380:BB36EC5B9459D563DB172F9284632648@n89.gzproxy.10155.com:8089/
-  with open('output/sub.txt','w') as f:
+  with open('docs/sub.txt','w') as f:
     f.write(socksurl)
